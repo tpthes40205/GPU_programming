c93f2ecfdeea7fe51f63f8b3ab931ff5aec45026
tpthes40205 <user@2729-yuch-nb2.epa.gov.tw>
2017-02-24 17:48:55 +0800

    GPU lab0 ver 1

--- lab0/main.cu
+++ lab0/main.cu
@@ -13,23 +13,37 @@
 const int W = 40;
 const int H = 12;
 
+__device__ __constant__ const strln[][2] = {"  Give me the permission key,        ","                            hooman   "};
+__device__ __constant__ const face[][3] = {"/\\___/\\"," o   o ","  =^=  "};
+
 __global__ void Draw(char *frame) {
 	// TODO: draw more complex things here
-	// Do not just submit the original file provided by the TA!
 	const int y = blockIdx.y * blockDim.y + threadIdx.y;
 	const int x = blockIdx.x * blockDim.x + threadIdx.x;
-	if (y < H and x < W) {
+	if (y < H && x < W) {
 		char c;
 		if (x == W-1) {
 			c = y == H-1 ? '\0' : '\n';
-		} else if (y == 0 or y == H-1 or x == 0 or x == W-2) {
+		}else if (y == 0 || y == H-1 || x == 0 || x == W-2){
 			c = ':';
-		} else {
+		}else if (y == 2 || y == 3){
+			c = strln[x-1][y-2];
+		}else if (x == 3 && y > 5){
+			c = '(';
+		}else if (y > 5 && (x-y)==5){
+			c = ')';
+		}else if (y > 4 && y < 8 && x > 2 && x < 11){
+			c = face[x-3][y-5];
+		}else if (y==10 && x > 15 && x < 26){
+			c = ')';
+		}else{
 			c = ' ';
 		}
 		frame[y*W+x] = c;
 	}
 }
+
+
 
 int main(int argc, char **argv)
 {
